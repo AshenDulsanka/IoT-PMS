c468026d1d432fc352f6d158a9cb20a382e4d4b9
Ashen <ashendul@gmail.com>
2024-03-04 16:17:33 +0530

    added wifi module

--- assets/sensorData/sensorData/src/main.cpp
+++ assets/sensorData/sensorData/src/main.cpp
@@ -1,8 +1,8 @@
 #include <Arduino.h>
 // libraries for temp sensor
 #include <OneWire.h>
-#include <DallasTemperature.h>
-// libraries for temp sensor ends here
+// including wifi libraries
+#include <WiFi.h>
 
 #define LED 2
 int oneWireBus = 14;    // temp sensor
@@ -11,16 +11,35 @@ int sound_digital = 25; // sound sensor
 
 // setting up onewire and dalls for temp sensor
 OneWire oneWire(oneWireBus);
-DallasTemperature sensors(&oneWire);
 // setting up done
+
+// wifi credentials
+const char *ssid = "yourNetworkName";
+const char *password = "yourNetworkPassword";
 
 void setup()
 {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(sound_digital, INPUT);
+  pinMode(TEMP_PIN, INPUT);
   pinMode(smoke_digital, INPUT);
   sensors.begin();
+
+  // connecting wifi
+  WiFi.mode(WIFI_STA); // Optional
+  WiFi.begin(ssid, password);
+  Serial.println("\nConnecting");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(100);
+  }
+
+  Serial.println("\nConnected to the WiFi network");
+  Serial.print("Local ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop()
@@ -34,14 +53,108 @@ void loop()
   // smoke sensor ends here
 
   // temp sensor
-  //  Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
-  sensors.requestTemperatures();
+  double readTempValue()
+  {
+    byte i;
+    byte present = 0;
+    byte type_s;
+    byte data[9];
+    byte addr[8];
+    float celsius, fahrenheit;
 
-  Serial.print("Celsius temperature: ");
-  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
-  Serial.println(sensors.getTempCByIndex(0));
-  delay(1000);
-  // temp sensor ends here  
+    if (!ds.search(addr))
+    {
+      // Serial.println("No more addresses.");
+      // Serial.println();
+      ds.reset_search();
+      delay(500);
+      return readTempValue();
+    }
+
+    if (OneWire::crc8(addr, 7) != addr[7])
+    {
+      Serial.println("CRC is not valid!");
+      return -1;
+    }
+
+    // the first ROM byte indicates which chip
+    switch (addr[0])
+    {
+    case 0x10:
+      // Serial.println("  Chip = DS18S20");  // or old DS1820
+      type_s = 1;
+      break;
+    case 0x28:
+      // Serial.println("  Chip = DS18B20");
+      type_s = 0;
+      break;
+    case 0x22:
+      // Serial.println("  Chip = DS1822");
+      type_s = 0;
+      break;
+    default:
+      Serial.println("Device is not a DS18x20 family device.");
+      return -1;
+    }
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 1); // start conversion, with parasite power on at the end
+
+    delay(1000); // maybe 750ms is enough, maybe not
+    // we might do a ds.depower() here, but the reset will take care of it.
+
+    present = ds.reset();
+    ds.select(addr);
+    ds.write(0xBE); // Read Scratchpad
+
+    // Serial.print("  Data = ");
+    // Serial.print(present, HEX);
+    // Serial.print(" ");
+    for (i = 0; i < 9; i++)
+    { // we need 9 bytes
+      data[i] = ds.read();
+    }
+    // Serial.print(" CRC=");
+    // Serial.print(OneWire::crc8(data, 8), HEX);
+    // Serial.println();
+
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s)
+    {
+      raw = raw << 3; // 9 bit resolution default
+      if (data[7] == 0x10)
+      {
+        // "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - data[6];
+      }
+    }
+    else
+    {
+      byte cfg = (data[4] & 0x60);
+      // at lower res, the low bits are undefined, so let's zero them
+      if (cfg == 0x00)
+        raw = raw & ~7; // 9 bit resolution, 93.75 ms
+      else if (cfg == 0x20)
+        raw = raw & ~3; // 10 bit res, 187.5 ms
+      else if (cfg == 0x40)
+        raw = raw & ~1; // 11 bit res, 375 ms
+      //// default is 12 bit resolution, 750 ms conversion time
+    }
+    celsius = (float)raw / 16.0;
+    fahrenheit = celsius * 1.8 + 32.0;
+    Serial.print("Temperature: ");
+    Serial.print(celsius);
+    Serial.print(" Celsius, ");
+    Serial.print(fahrenheit);
+    Serial.println(" Fahrenheit");
+    return (double)celsius;
+  }
+  // temp sensor ends here
 
   // sound sensor
   int soundValue = analogRead(sound_digital);
