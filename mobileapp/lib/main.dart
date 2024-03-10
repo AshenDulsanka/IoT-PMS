61b1d04bd1ddd2f3877acdf7b6ba56082bae02e4
Ashen Abeysekara <ashendul@gmail.com>
2024-03-10 18:55:46 +0530

    added poppin font

--- mobileapp/lib/main.dart
+++ mobileapp/lib/main.dart
@@ -1,4 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp());
+void main() => runApp(MaterialApp(
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text("UpTime"),
+      centerTitle: true,
+      backgroundColor: Colors.grey[800],
+    ),
+    body: Center(
+      child: Text(
+        "Hello user",
+        style: TextStyle(
+          fontSize: 40.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 10,
+          color: Colors.grey[700],
+
+        ),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text("click"),
+      backgroundColor: Colors.white10,
+    ),
+  ),
+));
 
