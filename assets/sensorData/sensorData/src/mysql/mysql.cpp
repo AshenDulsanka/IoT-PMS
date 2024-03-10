029f99c6c9abe9a0c83e6c8513821e3b3e2af692
Ashen Abeysekara <ashendul@gmail.com>
2024-03-10 15:42:03 +0530

    fixed sql codes and still fixing to send data to sql db

--- assets/sensorData/sensorData/src/mysql/mysql.cpp
+++ assets/sensorData/sensorData/src/mysql/mysql.cpp
@@ -1,10 +1,10 @@
 #include "mysql.h"
 
-const String URL = "https://uptimesensordata.000webhostapp.com/sensordata.php";
+const String URL = "http://192.168.1.77/pusl2022UpTime/sensordata.php";
 
 void sendDataToMySQL(double tempValue, int soundValue, int smokeValue, double fuelLvlValue, int vibrationValue, int currentValue, int oilPressureValue)
 {
-    String postData = "temp=" + String(tempValue) + "&sound=" + String(soundValue) + "&smoke=" + String(smokeValue) + "&fuelLvl=" + String(fuelLvlValue) + "&vibration=" + String(vibrationValue) + "&current=" + String((int)currentValue) + "&oilPressure=" + String(oilPressureValue);
+    String postData = "temp=" + String(tempValue) + "&sound=" + String(soundValue) + "&smoke=" + String(smokeValue) + "&fuelLvl=" + String(fuelLvlValue) + "&vibration=" + String(vibrationValue) + "&current=" + String(currentValue) + "&oilPressure=" + String(oilPressureValue);
     HTTPClient http;
     http.begin(URL);
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
