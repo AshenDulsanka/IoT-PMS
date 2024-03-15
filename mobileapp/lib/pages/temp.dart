2feaf3fe0b9d5141965afa58ca133f5d7c164a70
Ashen Abeysekara <ashendul@gmail.com>
2024-03-15 17:12:21 +0530

    modified temp page

--- mobileapp/lib/pages/temp.dart
+++ mobileapp/lib/pages/temp.dart
@@ -10,7 +10,7 @@ class Temp extends StatelessWidget {
       backgroundColor: Colors.grey[900],
       body: Center(
         child: Container(
-          padding: EdgeInsets.all(20.0),
+          padding: EdgeInsets.fromLTRB(20.0, 220.0, 20.0, 0.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
@@ -32,15 +32,15 @@ class Temp extends StatelessWidget {
               ),
               SizedBox(height: 80),
               Text(
-                "Temperature: 25°C",
+                "25 °C",
                 style: TextStyle(
-                  fontSize: 18,
+                  fontSize: 50,
                   fontWeight: FontWeight.normal,
                   color: Colors.white,
                   fontFamily: "Poppins",
                 ),
               ),
-              SizedBox(height: 20),
+              SizedBox(height: 40),
               Text(
                 "Status: Normal",
                 style: TextStyle(
