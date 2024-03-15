1a611cf5366b8def43438dbb9c82d5d6d1429db8
Ashen Abeysekara <ashendul@gmail.com>
2024-03-15 17:48:02 +0530

    modified gas page with appbar

--- mobileapp/lib/pages/gas.dart
+++ mobileapp/lib/pages/gas.dart
@@ -8,38 +8,52 @@ class Gas extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[900],
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.fromLTRB(20.0, 220.0, 20.0, 0.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Image(
-                image: AssetImage("assets/gas.png"),
-                width: 200,
-                height: 200,
-                alignment: Alignment.topCenter,
-              ),
-              Text(
-                "Gas Level",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontFamily: "Poppins",
+      appBar: AppBar(
+        title: Text(
+          "Gas Level",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[900], // Set app bar color to match background
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),// Remove elevation
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.fromLTRB(20.0, 190.0, 20.0, 0.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Image(
+                  image: AssetImage("assets/gas.png"),
+                  width: 200,
+                  height: 200,
+                  alignment: Alignment.topCenter,
                 ),
-              ),
-              SizedBox(height: 80),
-              Text(
-                "Status: Normal",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.white,
-                  fontFamily: "Poppins",
+                SizedBox(height: 10),
+                Text(
+                  "Status: Normal",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                    fontFamily: "Poppins",
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
