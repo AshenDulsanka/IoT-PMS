f15125ffd4cb927f0c3408fbb61cdde304c811e9
Ashen Abeysekara <ashendul@gmail.com>
2024-03-30 14:52:25 +0530

    added fb packages for temp

--- mobileapp/lib/pages/temp.dart
+++ mobileapp/lib/pages/temp.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class Temp extends StatelessWidget {
   const Temp({Key? key}) : super(key: key);
