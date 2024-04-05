b4ca52b9f3dceea7349a7323a83966aa778c94f1
Ashen Abeysekara <ashendul@gmail.com>
2024-04-05 19:28:36 +0530

    removed unwanted packages

--- mobileapp/lib/main.dart
+++ mobileapp/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'firebase_options.dart';
 import 'pages/home.dart';
 
 void main() async {
