d5d938d707626b55363e01a7512a38b30f79967e
Ashen Abeysekara <ashendul@gmail.com>
2024-04-06 20:47:15 +0530

    fixed some warnings on home page

--- mobileapp/lib/pages/home.dart
+++ mobileapp/lib/pages/home.dart
@@ -11,7 +11,7 @@ import './vibration.dart';
 import './nextmd.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
