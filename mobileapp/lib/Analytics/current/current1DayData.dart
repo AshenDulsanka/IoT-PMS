807c6fdc68e17fc29dad19114b0d22ce1a21e0bd
Ashen Abeysekara <ashendul@gmail.com>
2024-04-08 01:00:19 +0530

    added 1 day data to current page

--- mobileapp/lib/Analytics/current/current1DayData.dart
+++ mobileapp/lib/Analytics/current/current1DayData.dart
@@ -1,20 +1,20 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-class PricePoint {
+class DayCurrentData {
   final double x;
   final double y;
 
-  PricePoint({required this.x, required this.y});
+  DayCurrentData({required this.x, required this.y});
 }
 
-Future<List<PricePoint>> get1DayCurrentData() async {
+Future<List<DayCurrentData>> get1DayCurrentData() async {
   final url = Uri.parse('https://uptimesensordata.000webhostapp.com/current-1day-data.php');
   final response = await http.get(url);
 
   if (response.statusCode == 200) {
     final data = jsonDecode(response.body) as List;
-    return data.asMap().entries.map((entry) => PricePoint(x: entry.key.toDouble(), y: double.parse(entry.value.toString()))).toList();
+    return data.asMap().entries.map((entry) => DayCurrentData(x: entry.key.toDouble(), y: double.parse(entry.value.toString()))).toList();
   } else {
     throw Exception('Failed to fetch data from the PHP script');
   }
