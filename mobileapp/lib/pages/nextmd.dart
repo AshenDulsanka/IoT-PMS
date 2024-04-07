adef7bfd1bc02dcf70fad621461566183e1759d1
Ashen Abeysekara <ashendul@gmail.com>
2024-04-08 04:01:25 +0530

    added refresh to next md page

--- mobileapp/lib/pages/nextmd.dart
+++ mobileapp/lib/pages/nextmd.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'next_maintenance_date.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 
 class NextMD extends StatelessWidget {
   const NextMD({super.key});
+
+  Future<void> _handleRefresh() async {
+    await Future.delayed(const Duration(seconds: 1));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,30 +34,33 @@ class NextMD extends StatelessWidget {
           },
         ),// Remove elevation
       ),
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            padding: const EdgeInsets.fromLTRB(20.0, 150.0, 20.0, 0.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Image(
-                  image: AssetImage("assets/next_md.png"),
-                  width: 200,
-                  height: 200,
-                  alignment: Alignment.topCenter,
-                ),
-                const SizedBox(height: 80),
-                Text(
-                  "${NextMaintenanceDateManager.nextMaintenanceDays} Days",
-                  style: const TextStyle(
-                    fontSize: 40,
-                    fontWeight: FontWeight.normal,
-                    color: Colors.black,
-                    fontFamily: "Poppins",
+      body: LiquidPullToRefresh(
+        onRefresh: _handleRefresh,
+        child: SafeArea(
+          child: Center(
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(20.0, 150.0, 20.0, 0.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Image(
+                    image: AssetImage("assets/next_md.png"),
+                    width: 200,
+                    height: 200,
+                    alignment: Alignment.topCenter,
                   ),
-                ),
-              ],
+                  const SizedBox(height: 80),
+                  Text(
+                    "${NextMaintenanceDateManager.nextMaintenanceDays} Days",
+                    style: const TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.normal,
+                      color: Colors.black,
+                      fontFamily: "Poppins",
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
